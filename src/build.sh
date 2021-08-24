16ba8ce02f432948bffd7e28eb48ae7f0d050a31
Tao Liu <liutao17@ict.ac.cn>
2021-08-24 20:55:56 +0800

    update build.sh

--- src/build.sh
+++ src/build.sh
@@ -8,10 +8,14 @@ cd ../fit
 make
 cd ../fortran_code
 make
-cd bin
+cd ../../bin
 ln -s ../pre_data/mlff.py .
 ln -s ../pre_data/seper.py .
 ln -s ../pre_data/gen_data.py .
 ln -s ../pre_data/data_loader_2type.py .
+chmod +x ./mlff.py
+chmod +x ./seper.py
+chmod +x ./gen_data.py
+chmod +x ./data_loader_2type.py
 cd ..
 
