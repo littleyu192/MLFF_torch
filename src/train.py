d8fbe3cb645dfb5f84ae31ef680a17a845e89346
Tao Liu <liutao17@ict.ac.cn>
2021-09-12 09:46:40 -0400

    working snapshot

--- src/train.py
+++ src/train.py
@@ -295,7 +295,7 @@ loader_valid = Data.DataLoader(torch_valid_data, batch_size=1, shuffle=True)
 # ==========================part2:指定模型参数==========================
 n_epoch = 2000
 learning_rate = 0.8
-weight_decay = 0.9
+weight_decay = 1.0
 weight_decay_epoch = 100
 direc = './FC3model_mini_force'
 if not os.path.exists(direc):
@@ -385,6 +385,7 @@ if pm.isNNfinetuning == True:
     # if torch.cuda.device_count() > 1:
         # model = nn.DataParallel(model)
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    #optimizer = optim.SGD(model.parameters(), lr=learning_rate)
     scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=weight_decay)
     start = time.time()
     min_loss = np.inf
