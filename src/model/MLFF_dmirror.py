701c56a5759a54bd32855bfc8e515cdc6bece71e
Tao Liu <liutao17@ict.ac.cn>
2021-09-17 22:46:03 -0400

    BUGFIX:
    dmirror_linear uses incorrect bias shape with torch.randn(1),
    fix to shape torch.randn(out_dim)

--- src/model/MLFF_dmirror.py
+++ src/model/MLFF_dmirror.py
@@ -39,12 +39,12 @@ class MLFF_dmirror(nn.Module):
         #print("111111111111111111")
 
     def forward(self, image, dfeat, neighbor, Egroup_weight, divider):
-        print("defat.shape= ", dfeat.shape)
-        print("neighbor.shape = ", neighbor.shape)
-        print("dump dfeat ------------------->")
-        print(dfea)
-        print("dump neighbor ------------------->")
-        print(neighbor)
+        #print("defat.shape= ", dfeat.shape)
+        #print("neighbor.shape = ", neighbor.shape)
+        #print("dump dfeat ------------------->")
+        #print(dfeat)
+        #print("dump neighbor ------------------->")
+        #print(neighbor)
         #print("2222222222222222222")
         batch_size = image.shape[0]
         #print(batch_size)
