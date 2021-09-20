1) implement correct Force calculation in MLFF_dmirror.py 2) add weight decay option in Adam, only apply to 'non-bias' params 3) dmirror.py use traditional param name: weight and bias

src/model/MLFF_dmirror.py
@@ -44,8 +44,6 @@ class MLFF_dmirror(nn.Module):
             self.net = dmirror_FC(self.net_cfg, F.softplus, F.sigmoid)
         else:
             raise RuntimeError("MLFF_dmirror: unsupported activation_type: %s" %activation_type)
-        #print(self.natoms)
-        #print("111111111111111111")
 
     def forward(self, image, dfeat, neighbor, Egroup_weight, divider):
         batch_size = image.shape[0]
@@ -56,6 +54,7 @@ class MLFF_dmirror(nn.Module):
             (batch_size, self.natoms, self.dim_feat)
         ).to(device)
 
+        # FIXME: loops should be eliminated by matmul style network impl
         for batch_idx in range(batch_size):
             for i in range(self.natoms):
                 Ei, dEi_dFeat = self.net(image[batch_idx, i, :])
@@ -64,16 +63,31 @@ class MLFF_dmirror(nn.Module):
 
         Etot = torch.sum(result_Ei, 1)
         Force = torch.zeros((batch_size, self.natoms, 3)).to(device)
-        result_dEtot_dFeat = result_dEi_dFeat.view(
-            [batch_size, self.natoms * self.dim_feat]
-        )
 
+        # here we use the infinite cell (in Rcut) view to calc F_atom_i
+        # the formula is: sum dE(all neighbor atoms in Rcut)/dR_(this atom)
+        #
+        # for all infinite cells, the calculate of dE(atom)/dFeat is same as
+        # the central unit cell, since they are just duplicates of unit cell,
+        # but each cell should has its' own version of dFeat/dRi_(unit_cell),
+        # so we will have
+        #
+        # atom_idx     :  to index dE(atom)/dFeat
+        # neighbor_idx :  to index dFeat/dRi_(unit_cell)
+        #
         for batch_idx in range(batch_size):
             for i in range(self.natoms):
-                a = result_dEtot_dFeat[batch_idx].unsqueeze(0)
-                b0 = dfeat[batch_idx, i, :self.natoms]  # FIXME: this is fool?
-                b = b0.view([self.natoms * self.dim_feat, 3])
-                Force[batch_idx, i, :] = torch.mm(a, b)
+                # get atom_idx & neighbor_idx
+                my_neighbor = neighbor[batch_idx, i]
+                neighbor_idx = my_neighbor.nonzero().squeeze().type(torch.int64)
+                atom_idx = my_neighbor[neighbor_idx].type(torch.int64) - 1
+                # calculate Force
+                #   a.shape = [neighbor_num, 1, self.dim_feat]
+                #   b.shape = [neighbor_num, self.dim_feat, 3]
+                #   Force.shape = [batch_size, self.natoms, 3]
+                a = result_dEi_dFeat[batch_idx, atom_idx].unsqueeze(1)
+                b = dfeat[batch_idx, i, neighbor_idx]
+                Force[batch_idx, i, :] = torch.matmul(a, b).sum([0, 1])
 
         return Etot, Force
 

src/train.py
@@ -41,10 +41,11 @@ opt_step = 0
 opt_act = 'sigmoid'
 opt_dtype = pm.training_dtype
 opt_net_cfg = 'default'
+opt_regular_wd = float(0)
 
 opts,args = getopt.getopt(sys.argv[1:],
-    '-h-v-s-e:-l:-g:-t:-a:-d:-n:',
-    ['help','verbose','summary','epochs=','lr=','gamma=','step=','act=', 'dtype=', 'net_cfg='])
+    '-h-v-s-e:-l:-g:-t:-a:-d:-n:-w:',
+    ['help','verbose','summary','epochs=','lr=','gamma=','step=','act=', 'dtype=', 'net_cfg=', 'weight_decay='])
 
 for opt_name,opt_value in opts:
     if opt_name in ('-h','--help'):
@@ -62,6 +63,7 @@ for opt_name,opt_value in opts:
         print("     -d dtype, --dtype=dtype     :  specify default dtype: [float64, float32]")
         print("     -n cfg, --net_cfg=cfg       :  specify network cfg variable in parameters.py")
         print("                                    eg: -n MLFF_dmirror_cfg1")
+        print("     -w val, --weight_decay=val  :  specify weight decay regularization value")
         print("")
         exit()
     elif opt_name in ('-v','--verbose'):
@@ -82,6 +84,8 @@ for opt_name,opt_value in opts:
         opt_dtype = opt_value
     elif opt_name in ('-n','--net_cfg'):
         opt_net_cfg = opt_value
+    elif opt_name in ('-w','--weight_decay'):
+        opt_regular_wd = float(opt_value)
 
 # set default training dtype
 #
@@ -175,10 +179,12 @@ def train(sample_batches, model, optimizer, criterion, last_epoch):
     if (opt_verbose == True):
         print("defat.shape= ", dfeat.shape)
         print("neighbor.shape = ", neighbor.shape)
+        #torch.set_printoptions(profile="full")
         print("dump dfeat ------------------->")
         print(dfeat)
         print("dump neighbor ------------------->")
         print(neighbor)
+        #torch.set_printoptions(profile="default")
 
     model = model.to(device)
     # model = model.cuda()
@@ -246,6 +252,11 @@ def train(sample_batches, model, optimizer, criterion, last_epoch):
     #
     # Etot_label.shape = [batch_size, 1], while Etot_predict.shape = [batch_size], so squeeze Etot_label to match
     #
+    if (opt_verbose or (opt_summary and last_epoch)):
+        for name, parameter in model.named_parameters():
+            print("dump model parameter (%s : %s) -------->" %(name, parameter.size()))
+            print(parameter)
+
     loss = pm.rtLossF * criterion(Force_predict, Force_label) + pm.rtLossEtot * criterion(Etot_predict, Etot_label.squeeze())
     loss_F = criterion(Force_predict, Force_label)
     loss_Etot = criterion(Etot_predict, Etot_label.squeeze())
@@ -379,10 +390,16 @@ LR_step = 100
 if (opt_step != 0):
     LR_step = opt_step
 
+# for Regularization
+REGULAR_wd = 0.
+if (opt_regular_wd != 0.):
+    REGULAR_wd = opt_regular_wd
+
 print("Training: n_epoch = %d" %n_epoch)
 print("Training: LR_base = %.16f" %LR_base)
 print("Training: LR_gamma = %.16f" %LR_gamma)
 print("Training: LR_step = %d" %LR_step)
+print("Training: REGULAR_wd = %.16f" %REGULAR_wd)
 
 #LR_milestones = [1000, 1500, 1700, 1800, 1900, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
 #LR_tmax=200
@@ -484,8 +501,13 @@ if pm.isNNfinetuning == True:
 
     # if torch.cuda.device_count() > 1:
         # model = nn.DataParallel(model)
-    #optimizer = optim.Adam(model.parameters(), lr=LR_base)
-    optimizer = optim.SGD(model.parameters(), lr=LR_base, momentum=0.5)
+    optimizer = optim.Adam(
+                [
+                    {'params': (p for name, p in model.named_parameters() if 'bias' not in name)},
+                    {'params': (p for name, p in model.named_parameters() if 'bias' in name), 'weight_decay': 0.}
+                ],
+                lr=LR_base, weight_decay = REGULAR_wd)
+    #optimizer = optim.SGD(model.parameters(), lr=LR_base, momentum=0.5)
     #scheduler = optim.lr_scheduler.OneCycleLR(
     #                            optimizer, max_lr=LR_max, 
     #                            steps_per_epoch=LR_steps, epochs=LR_epochs)