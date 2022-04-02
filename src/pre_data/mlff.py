6734fbf101c1d96a5f50ec6a3ae9500c708573b4
wlj-pwmat <98017326+wlj-pwmat@users.noreply.github.com>
2022-04-02 10:40:13 +0800

    Update mlff.py

--- src/pre_data/mlff.py
+++ src/pre_data/mlff.py
@@ -148,10 +148,12 @@ if hasattr(pm, 'isNewMd100'):
         os.system('rm -f MOVEMENT')
         sys.path.append(codepath+'/../test')
         import md100
-        imodel = 3    # 1:linear;  2:VV;   3:NN;
-        num_process = 1
-        if hasattr(pm, 'md100_imodel'):
-            imodel = pm.md100_imodel
+        num_process = 1                  #use 1 mpi process as default
+        
+        if hasattr(pm, 'imodel'):
+            imodel = pm.imodel
+        else:
+            print("Specification of parameter imodel is required!")
         if hasattr(pm, 'md_num_process'):
             num_process = pm.md_num_process
         md100.run_md100(imodel=imodel, atom_type=pm.atomType, num_process=num_process)
