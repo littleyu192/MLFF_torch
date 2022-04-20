e7f1b00040bc664a1d0ec8832912122d52e3d7b6
xiaoHU <littleyu192@163.com>
2022-04-20 17:18:41 +0800

    rm redundency code

--- src/build.sh
+++ src/build.sh
@@ -4,8 +4,11 @@ mkdir bin
 mkdir lib
 make -C pre_data/gen_feature
 make -C pre_data/fit
-make -C pre_data/fortran_code  # this is *.so for tf inference, deprecated
+make -C pre_data/fortran_code  # spack load gcc@7.5.0
 make -C test/MD
+cd QCAD/fortran_code
+make libiotk.a
+cd ../..
 make -C QCAD/fortran_code
 cd bin
 ln -s ../pre_data/mlff.py .
